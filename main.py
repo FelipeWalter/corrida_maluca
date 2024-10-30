183471ccb90b4256d3b8fe3ef4e1ef1d9ab2773e
Felipe Walter <felipewalter708@gmail.com>
2024-10-29 21:11:34 -0300

    Carros Adicionados

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ clock = pygame.time.Clock()
 pygame.display.set_caption('Corrida Maluca')
 branco = (255,255,255)
 preto = (0,0,0)
+fundo = pygame.image.load("assets/assets/fundo.png")
+carro1 = pygame.image.load("assets/assets/carro1.png")
+carro2 = pygame.image.load("assets/assets/carro2.png")
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             quit()
 
     tela.fill(branco)
+    tela.blit(fundo, (0,0))
+    tela.blit(carro1, (0,50))
+    tela.blit(carro2, (0,180))
     pygame.display.update()
     clock.tick(60)
 
